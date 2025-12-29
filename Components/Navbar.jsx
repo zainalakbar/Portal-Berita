b04e3895fbcd17cf702a99e5f908bfa313211898
Akbarre <akbarzainalapp@gmail.com>
2025-12-30 01:35:27 +0700

    commit ke 2

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,6 +2,18 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 function MyNavbar() {
     return (
-        <Navbar bg=
-    )
+        <Navbar bg="dark" variant="dark" expand="ig" className="stick-top shadow-sn">
+            <Container>
+                <Navbar.Brand href="/">BeritaOline</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/katagori/teknologi">Teknologi</Nav.Link>
+                    <Nav.Link href="/kategori/olahraga">Olahraga</Nav.Link>
+                    <Nav.Link href="/kategori/politik">Politik</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 }
+
+export default MyNavbar;
