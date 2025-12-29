f2cb9ba425776536a7af66b52ecd618ac4cff5b9
Akbarre <akbarzainalapp@gmail.com>
2025-12-30 01:47:06 +0700

    commit ke 3

---
+++ Components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-light text-center py-3 nt-4 w-100">
+            <Container>
+                <small>@ 2025 BeritaOnline | Di Buat Dengan React Js & Bootstrap</small>
+            </Container>
+        </footer>
+    );
+}
+
+export default Footer;
